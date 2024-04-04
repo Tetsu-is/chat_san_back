6533547b985e2fa1113252449dc66580f1b1a9b4
Tetsu-is <hap30305@kwansei.ac.jp>
2024-04-05 01:05:58 +0900

    define router

--- handler/router/router.go
+++ handler/router/router.go
@@ -1,1 +1,15 @@
 package router
+
+import (
+	"database/sql"
+	"net/http"
+)
+
+func NewRouter(db *sql.DB) *http.ServeMux {
+	msgSrv := service.NewMessageService(db)
+
+	mux := http.NewServeMux()
+	mux.Handle("/messages", handler.NewMessageHandler(msgSrv))
+
+	return mux
+}
--- main.go
+++ main.go
@@ -1,5 +1,62 @@
 package main
 
+import (
+	"chat_san/db"
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
 func main() {
-	// Code
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer stop()
+
+	const (
+		defaultPort   = ":8080"
+		defaultDBPath = ".sqlite3/chat.db"
+	)
+
+	db, err := db.NewDB(defaultDBPath)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	mux := router.NewRouter(db)
+
+	server := http.Server{
+		Addr:    defaultDBPath,
+		Handler: mux,
+	}
+
+	errCh := make(chan error, 1)
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			errCh <- err
+
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		shutdownWithTimeout(&server)
+	case err := <-errCh:
+		log.Printf("Error: %v", err)
+	}
+
+}
+
+func shutdownWithTimeout(srv *http.Server) {
+	//Contextを作成し、30秒のTimeoutを設定
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	//Timeoutを設定したContextを渡すことで無期限に待機しないようにする
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
